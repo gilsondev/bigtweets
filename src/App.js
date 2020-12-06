3f9500fa2a6e659faa4fa39ae004fe8afdb37432
Gilson Filho <me@gilsondev.in>
2020-12-06 13:29:54 -0300

    Config react router and create header and home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import React from 'react';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
